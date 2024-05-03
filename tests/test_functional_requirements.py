44a1a652ee1efc283beb342eeb11d5d97c32f652
Ali Waheed <aliwaheed.aw@outlook.com>
2024-05-03 12:58:19 -0400

    Added unit-test .py file

---
+++ tests/test_functional_requirements.py
@@ -0,0 +1,20 @@
+import pytest
+import os
+
+@pytest.fixture
+def get_path_figures():
+    return "./reports/figures"
+
+@pytest.fixture
+def get_path_models():
+    return "./models"
+
+def test_figure_files(get_path_figures):
+    assert 'feature_importance.png' in os.listdir(get_path_figures)
+    assert 'residuals.png' in os.listdir(get_path_figures)
+
+def test_metric_file(get_path_figures):
+    assert 'metrics.txt' in os.listdir(get_path_figures.rsplit('/',1)[0])
+
+def test_model_file(get_path_models):
+    assert 'model.skops' in os.listdir(get_path_models)
