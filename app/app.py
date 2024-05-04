63fa323a34d31a81536c1fdc3040c62d32259311
Ali Waheed <aliwaheed.aw@outlook.com>
2024-05-04 11:50:47 -0400

    Added relevant resources

--- app/app.py
+++ app/app.py
@@ -1,46 +1,34 @@
 import streamlit as st
-import skops.io as sio
+import spacy
+from spacy import displacy
 
-model = sio.load("./model.skops", trusted=True)
+# Load the spaCy model
+nlp = spacy.load("en_legal_ner_sm")
 
-def predict_score(
-        fixed_acidity, volatile_acidity, citric_acid, residual_sugar, 
-        chlorides, free_sulfur_dioxide, total_sulfur_dioxide, density, 
-        ph, sulphates, alcohol
-        ):
+# Function to extract entities from text
+def extract_entities(text):
+    doc = nlp(text)
+    entities = [(ent.text, ent.label_) for ent in doc.ents]
+    return entities, doc
 
-    features = [fixed_acidity, volatile_acidity, citric_acid, residual_sugar, 
-    chlorides, free_sulfur_dioxide, total_sulfur_dioxide, density, 
-    ph, sulphates, alcohol]
-
-    predicted_quality = model.predict([features])[0]
-
-    score = f"Predicted Quality Score: {int(round(predicted_quality, 0))}"
-    return score
-
+# Streamlit app
 def main():
-    st.title("Legal NER Classifier")
-    st.write("Enter the details to correctly identify Legal Classes")
+    st.title("Legal NER")
 
-    fixed_acidity = st.slider("Fixed Acidity", 4.0, 16.0, step=0.1)
-    volatile_acidity = st.slider("Volatile Acidity", 0.01, 2.0, step=0.01)
-    citric_acid = st.slider("Citric Acid", 0.0, 1.0, step=0.01)
-    residual_sugar = st.slider("Residual Sugar", 0.5, 16.0, step=0.1)
-    chlorides = st.slider("Chlorides", 0.001, 0.7, step=0.001)
-    free_sulfur_dioxide = st.slider("Free Sulfur Dioxide", 1.0, 72.0, step=1.0)
-    total_sulfur_dioxide = st.slider("Total Sulfur Dioxide", 5.0, 290.0, step=2.0)
-    density = st.slider("Density", 0.00001, 1.0, step=0.0001)
-    ph = st.slider("pH Value", 2.0, 4.0, step=0.01)
-    sulphates = st.slider("Sulphates", 0.3, 2.0, step=0.01)
-    alcohol = st.slider("Alcohol", 4.0, 16.0, step=0.1)
+    # Text input
+    text = st.text_area("Enter some text:")
 
-    if st.button("Predict"):
-        prediction = predict_score(
-            fixed_acidity, volatile_acidity, citric_acid, residual_sugar, 
-            chlorides, free_sulfur_dioxide, total_sulfur_dioxide, density, 
-            ph, sulphates, alcohol
-        )
-        st.write(prediction)
+    # Button to extract entities
+    if st.button("Extract Entities"):
+        if text.strip() != "":
+            entities, doc = extract_entities(text)
+            if entities:
+                html = displacy.render(doc, style="ent", page=False)
+                st.write(html, unsafe_allow_html=True)
+            else:
+                st.write("No entities found.")
+        else:
+            st.write("Please enter some text.")
 
 if __name__ == "__main__":
     main()
